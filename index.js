808e612f475ca3e528100e275842a5be84d6b1d2
Shashwat Kumar Singh <73544122+harryalbrus272@users.noreply.github.com>
2021-03-10 23:36:25 +0530

    readline module practice

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ console.log(tutorial.PI);
 console.log(new tutorial.SomeMathObject('class called'));
 //emitter is built-in to the node
 const EventEmitter = require('events');
-const { pseudoRandomBytes } = require('crypto');
+//instance of EventEmitter
 const eventEmitter = new EventEmitter();
-//only get executed if the event 'tutorial' occurs
+//only get executed if the event 'tutorial' occurs. It's the listener
 eventEmitter.on('tutorial', (num1,num2)=>{
     console.log(num1+num2);
 });
@@ -37,3 +37,39 @@ sam.on('name',()=>{
 //emit a event 
 sks.emit('name');
 sam.emit('name');
+
+
+//readline module
+const readLine = require('readline');
+const { rawListeners } = require('process');
+//createInterface takes in two argument object
+const r1 = readLine.createInterface({input:process.stdin, output: process.stdout});
+let num1 = Math.floor((Math.random()*10)+1);
+let num2 = Math.floor((Math.random()*10)+1);
+let answer = num1+num2;
+
+r1.question(`What is  ${num1} + ${num2} ?`, (userInput)=>{
+    if(userInput.trim() == answer)
+    {
+        r1.close();
+        //this emits an close event
+    } else{
+        r1.setPrompt(`Incorrect response of ${userInput}, please try again\n`);
+        r1.prompt();
+        r1.on('line',(userInput)=>{
+            if(userInput.trim() == answer){
+                r1.close();
+            } else{
+                r1.setPrompt(`Incorrect response of ${userInput}, please try again\n`);
+                r1.prompt(); 
+            }
+        });
+    }
+});
+
+//Here, we are listening to the close event emitted from above
+r1.on('close', ()=>{
+    console.log('Correct!');
+});
+
+
